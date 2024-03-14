dc0a71a657f23183723483ec59ce3378e7a7f1de
tcolbath <trevor.colbath@gmail.com>
2024-03-14 12:35:12 -0400

    block_to_block_type

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,5 +1,4 @@
 
-
 def markdown_to_blocks(markdown):
     blocks = []
     lines = markdown.split("\n\n")
@@ -10,4 +9,47 @@ def markdown_to_blocks(markdown):
         blocks.append(line)
     return blocks
 
+def block_to_block_type(block):
+    #Simple block types
+    headers = {
+        "# ": "head1",
+        "## ": "head2",
+        "### ": "head3",
+        "#### ": "head4",
+        "##### ": "head5",
+        "######": "head6",
+    }
+    for key in headers.keys():
+        if block.startswith(key):
+            return headers[key]
+    if block.startswith("```"):
+        if block.endswith("```"):
+            return "code"
+    
+    #More complex block types
+    prev_block_type = "init"
+    for i, line in enumerate(block.splitlines()):
+        all_lines = False
+        
+        #Block types
+        if line.startswith("> "):
+            block_type = "quote"
+            all_lines = True
+        if line.startswith("* ") or line.startswith("- "):
+            block_type = "ulist"
+            all_lines = True
+        if line.startswith(f"{i+1}."):
+            block_type = "olist"
+            all_lines = True
 
+        #Verify all lines are matching.
+        if prev_block_type != "init":
+            if block_type != prev_block_type:
+                all_lines = False
+        if all_lines == False:
+            break
+        prev_block_type = block_type
+
+    if all_lines == True:
+        return(block_type)
+    return("par")
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -1,5 +1,5 @@
 import unittest
-from block_markdown import markdown_to_blocks
+from block_markdown import markdown_to_blocks, block_to_block_type
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -17,6 +17,23 @@ This is the same paragraph on a new line
              ' the same paragraph on a new line', '* This is a list\n* with items'],
              markdown_to_blocks(markdown)
         )
+
+    def test_block_to_block_types(self):
+        block = "# heading"
+        self.assertEqual(block_to_block_type(block), "head1")
+        block = "```\ncode\n```"
+        self.assertEqual(block_to_block_type(block), "code")
+        block = "> quote\n> more quote"
+        self.assertEqual(block_to_block_type(block), "quote")
+        block = "* list\n* items"
+        self.assertEqual(block_to_block_type(block), "ulist")
+        block = "1. list\n2. items"
+        self.assertEqual(block_to_block_type(block), "olist")
+        block = "paragraph"
+        self.assertEqual(block_to_block_type(block), "par")
+
+
+
 
 if __name__ == "__main__":
     unittest.main()
