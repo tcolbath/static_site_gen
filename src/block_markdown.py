6cb070f02ffd50cdcb4ab48b8a4fd79420d6b561
tcolbath <trevor.colbath@gmail.com>
2024-03-14 15:39:44 -0400

    markdown_to_html_node

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,3 +1,7 @@
+from textnode import text_node_to_html_node
+from inline_markdown import text_to_textnodes
+from htmlnode import HTMLNode, ParentNode
+
 
 def markdown_to_blocks(markdown):
     blocks = []
@@ -9,15 +13,40 @@ def markdown_to_blocks(markdown):
         blocks.append(line)
     return blocks
 
+def markdown_to_html_node(markdown):
+    dispatch_to_html_node = {
+        "code": code_to_html_node,
+        "olist": olist_to_html_node,
+        "ulist": ulist_to_html_node,
+        "quote": quote_to_html_node,
+        "h1": heading_to_html_node,
+        "h2": heading_to_html_node,
+        "h3": heading_to_html_node,
+        "h4": heading_to_html_node,
+        "h5": heading_to_html_node,
+        "h6": heading_to_html_node,
+        "par": paragraph_to_html_node
+    }
+    blocks = markdown_to_blocks(markdown)
+    children = []
+    for block in blocks:
+        block_type = block_to_block_type(block)
+        if block_type not in dispatch_to_html_node.keys():
+            raise ValueError(f"Unknown Block Type: '{block_type}'")
+        html_node = dispatch_to_html_node[block_type](block)
+        children.append(html_node)
+    return ParentNode("div", children, None)
+
+
 def block_to_block_type(block):
     #Simple block types
     headers = {
-        "# ": "head1",
-        "## ": "head2",
-        "### ": "head3",
-        "#### ": "head4",
-        "##### ": "head5",
-        "######": "head6",
+        "# ": "h1",
+        "## ": "h2",
+        "### ": "h3",
+        "#### ": "h4",
+        "##### ": "h5",
+        "######": "h6",
     }
     for key in headers.keys():
         if block.startswith(key):
@@ -54,5 +83,63 @@ def block_to_block_type(block):
         return(block_type)
     return("par")
 
-def quote_to_html(block):
-    
+
+def text_to_children(text):
+    text_nodes = text_to_textnodes(text)
+    children = []
+    for text_node in text_nodes:
+        html_node = text_node_to_html_node(text_node)
+        children.append(html_node)
+    return children
+
+
+def paragraph_to_html_node(block):
+    lines = block.split("\n")
+    paragraph = " ".join(lines)
+    children = text_to_children(paragraph)
+    return ParentNode("p", children)
+
+
+def heading_to_html_node(block):
+    header_level = block_to_block_type(block)
+    text = block.lstrip("#").strip()
+    children = text_to_children(text)
+    return ParentNode(header_level, children)
+
+
+def code_to_html_node(block):
+    text = block[4:-3]
+    children = text_to_children(text)
+    code = ParentNode("code", children)
+    return ParentNode("pre", [code])
+
+
+def olist_to_html_node(block):
+    items = block.split("\n")
+    html_items = []
+    for item in items:
+        text = item[3:]
+        children = text_to_children(text)
+        html_items.append(ParentNode("li", children))
+    return ParentNode("ol", html_items)
+
+
+def ulist_to_html_node(block):
+    items = block.split("\n")
+    html_items = []
+    for item in items:
+        text = item[2:]
+        children = text_to_children(text)
+        html_items.append(ParentNode("li", children))
+    return ParentNode("ul", html_items)
+
+
+def quote_to_html_node(block):
+    lines = block.split("\n")
+    new_lines = []
+    for line in lines:
+        new_lines.append(line.lstrip(">").strip())
+    content = " ".join(new_lines)
+    children = text_to_children(content)
+    return ParentNode("blockquote", children)
+
