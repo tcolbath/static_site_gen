7726285f5413abd3e3bcd47b495769f9bba7f1e2
tcolbath <trevor.colbath@gmail.com>
2024-03-14 16:16:56 -0400

    copy_static

--- src/main.py
+++ src/main.py
@@ -1,8 +1,13 @@
 from textnode import TextNode, text_node_to_html_node, type_verify
 from htmlnode import HTMLNode, LeafNode, ParentNode
+import os, shutil
+
 
 def main():
     print("Work in progress")
 
+
+def copy_static():
+    pass
 
 main()
